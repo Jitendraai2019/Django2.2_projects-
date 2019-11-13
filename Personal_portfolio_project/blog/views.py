71b25b4c1fc3af9f4cc673572263f462550a3fe0
Jitendra <jitendraai2019@gmail.com>
2019-11-13 12:39:52 +0530

    first commit

---
+++ Personal_portfolio_project/blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from . models import Blog
+# Create your views here.
+def allblog(request):
+    blogs = Blog.objects
+    return render(request,'blog/allblog.html',{'blogs':blogs})
